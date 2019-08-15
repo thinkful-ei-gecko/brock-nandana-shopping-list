708f3af0975d07865d0ba8cb96b736b13b326422
nrrao <nandanarao@yahoo.com>
2019-08-15 17:45:07 -0400

    shopping list

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict';
+function addItem(){
+  
+  $('#js-shopping-list-form').submit(function(event) {
+    
+    event.preventDefault();
+    const item = $('#shopping-list-entry').val();
+    $('#shopping-list-entry').val('');
+    $('ul').append(`<li><span class="shopping-item">${item} </span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div> </li>`);
+    
+      });
+}
+
+function checkItem(){
+  $('.shopping-list').on('click','.shopping-item-toggle',function(){
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+}
+function main(){
+  addItem();
+  checkItem();
+}
+$(main);
